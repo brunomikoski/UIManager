162bfddc584c2815098c6f5f544f6bd55a999ac3
Bruno Mikoski <bruno.mikoski@bossastudios.com>
2023-07-25 15:37:16 +0100

    fix: removing collection name

--- Scripts/Runtime/Core/Database/Windows/UIWindowCollection.cs
+++ Scripts/Runtime/Core/Database/Windows/UIWindowCollection.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 namespace BrunoMikoski.UIManager
 {
-    [CreateAssetMenu(menuName = "Create UIWindowCollection", fileName = "UIWindowCollection", order = 0)]
     public sealed class UIWindowCollection : ScriptableObjectCollection<UIWindow>
     {
         
